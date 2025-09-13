a7232db2dc5feb1b31693ff18c6ba21ca7fba43b
Okpara007 <chinemeremokpara93@gmail.com>
2025-09-13 21:30:26 +0100

    added path rewrites

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: __dirname,
-
+  basePath: '/calculator', 
+  assetPrefix: '/calculator', 
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/calculator/:path*',  
+        destination: '/:path*', 
+      },
+    ];
+  },
 };
 
 export default nextConfig;
